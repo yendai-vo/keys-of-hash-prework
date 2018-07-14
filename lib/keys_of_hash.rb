3b8d363807bc9dcb80923efda726fc8a433f94d6
Cindy Vo <yendai.vo@hotmail.com>
2018-07-14 23:29:05 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    result = []
+    self.map { |animal, location|
+      if arguments.include?(location)
+        result.push(animal)
+      end
+    }
+    result
   end
 end
