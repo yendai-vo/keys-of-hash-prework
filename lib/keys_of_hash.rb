e8a73de9be88545dd0e3ec13d24932fbba7e90f7
Cindy Vo <yendai.vo@hotmail.com>
2018-07-14 23:24:42 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(*arguments)
     result = []
     self.map { |animal, location|
-      if arguments.includes?(location)
+      if arguments.include?(location)
         result.push(animal)
       end
     }
